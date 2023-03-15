29d4a0493750487dd5f7e5ac2b83f3c3b5e6d8f0
Pranav <pranav.joshi@iitgn.ac.in>
2023-03-15 20:15:32 +0530

    eraser

--- script.js
+++ script.js
@@ -1,62 +1,21 @@
 r = 3
 var can = document.getElementById('sketchpad');
+var glass = document.getElementById("glass")
+glass.width = can.width = innerWidth
+glass.height = can.height = innerHeight
 c = can.getContext('2d');
+g = glass.getContext('2d');
 c.lineWidth = 2*r
-function drawDot(c,x,y,size) {
+g.lineWidth = 2*r
+g.fillStyle = 'rgba(255,99,71,0.2)'
+g.strokeStyle = 'red'
+var X,Y,md=false;
+var pathX,pathY
+function Dot(c,x,y,size) {
     c.beginPath();
     c.arc(x, y, size, 0, Math.PI*2, true);
     c.closePath();
     c.fill();
-}
-var X,Y,md=false;
-can.onmousedown = function(e) {
-    md=true;
-    getPos(e)
-    c.beginPath()
-    c.moveTo(X,Y)
-}
-window.onmouseup = function(){
-    md=false;
-    c.stroke()
-}
-can.onmousemove= function(e){ 
-    getPos(e);
-    if (md) { 
-        c.lineTo(X,Y) 
-        c.stroke()
-        c.beginPath()
-        c.moveTo(X,Y)
-    }
-}
-
-//Touch
-
-var pathX,pathY
-can.ontouchstart = function(e) {
-    pathX=[];
-    pathY=[];
-    getPos(e);
-    drawDot(c,X,Y,r);
-    pathX.push(X)
-    pathY.push(Y)
-    e.preventDefault();
-}
-can.ontouchmove = function(e) { 
-    getPos(e);
-    drawDot(c,X,Y,r); 
-    pathX.push(X)
-    pathY.push(Y)
-    e.preventDefault();
-}
-window.ontouchend = function(){
-    let l = pathX.length
-    if(l===0){return;}
-    c.beginPath()
-    c.moveTo(pathX[0],pathY[0])
-    for(var i=1;i<l;i++){
-        c.lineTo(pathX[i],pathY[i])
-    }
-    c.stroke()
 }
 function getPos(e) {
     if (e.touches) {
@@ -75,3 +34,83 @@ function getPos(e) {
         Y = e.layerY;
     }
 }
+
+function drawMode(){
+
+c.fillStyle = 'black'
+//mouse
+can.onmousedown = function(e) {
+    md=true;
+    getPos(e)
+    c.beginPath()
+    c.moveTo(X,Y)
+}
+window.onmouseup = function(){
+    md=false;
+    c.stroke()
+}
+can.onmousemove = function(e){ 
+    getPos(e);
+    if (md) { 
+        c.lineTo(X,Y) 
+        c.stroke()
+        c.beginPath()
+        c.moveTo(X,Y)
+    }
+}
+
+//Touch
+can.addEventListener("touchstart" ,function(e) {
+    pathX=[];
+    pathY=[];
+    getPos(e);
+    Dot(c,X,Y,r);
+    pathX.push(X)
+    pathY.push(Y)
+    e.preventDefault();
+})
+can.addEventListener("touchmove",function(e) { 
+    getPos(e);
+    Dot(c,X,Y,r); 
+    pathX.push(X)
+    pathY.push(Y)
+    e.preventDefault();
+})
+window.addEventListener("touchend" ,function(){
+    let l = pathX.length
+    if(l===0){return;}
+    c.beginPath()
+    c.moveTo(pathX[0],pathY[0])
+    for(var i=1;i<l;i++){
+        c.lineTo(pathX[i],pathY[i])
+    }
+    c.stroke()
+})
+}
+
+var X0,Y0
+
+function clearMode(){
+can.ontouchstart = can.onmousedown = function(e) {
+    md=true;
+    getPos(e)
+    X0 = X
+    Y0 = Y
+}
+window.ontouchmove = window.onmouseup = function(e){
+    md=false;
+    getPos(e)
+    c.clearRect(X0,Y0,X-X0,Y-Y0)
+    g.clearRect(0,0,innerWidth,innerHeight)
+}
+can.ontouchend = can.onmousemove = function(e){ 
+    g.clearRect(X0,Y0,X-X0,Y-Y0)
+    getPos(e);
+    if (md) { 
+        c.moveTo(X,Y)
+        g.fillRect(X0,Y0,X-X0,Y-Y0)
+    }
+}
+}
+
+drawMode()
